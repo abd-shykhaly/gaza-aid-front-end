a14ec61d596acc67080394b9e571f2eb3b0763d8
abd-shykhaly <mohammedsaidissa1999@gmail.com>
2026-01-26 12:39:14 +0200

    Update vite.config.ts: fixed api

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/gaza-aid-front-end/",
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
+
+  return {
+    plugins: [react()],
+    base: "/gaza-aid-front-end",
+    build: {
+      outDir: "dist",
+      sourcemap: false,
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: isDev
+            ? "http://localhost:5000"
+            : "https://gaza-aid-exchange-api.onrender.com",
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
